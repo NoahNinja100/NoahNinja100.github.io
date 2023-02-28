4c92c06d569c3ad8bb9f18fedc1b9b5ae3e623a2
Noahninja100 <83727826+NoahNinja100@users.noreply.github.com>
2023-02-28 14:03:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -434,7 +434,7 @@ function animateUrlWithEmojis () {
 
       for (i = 0; i < 10; i++) {
         m = Math.floor(e.length * ((Math.sin((Date.now() / 100) + i) + 1) / 2))
-        s += '㋛' + e[m]
+        s += '👶' + e[m]
       }
 
       window.location.hash = s
@@ -456,7 +456,7 @@ function animateUrlWithEmojis () {
   }
 
   function animateUrlWithMoons () {
-    const f = ['☺', '☻', '☺', '☻', '☺', '☻', '☺', '☻']
+    const f = ['🌑', '🌘', '🌗', '🌖', '🌕', '🌔', '🌓', '🌒']
     const d = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     let m = 0
 
